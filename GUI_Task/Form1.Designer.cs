5202b460887832df8b8041fdc98cc8a18e56ba70
Fimedur <fimedur.rahman.shuvo@gmail.com>
2021-04-08 15:36:30 +0600

    done

--- GUI_Task/Form1.Designer.cs
+++ GUI_Task/Form1.Designer.cs
@@ -112,38 +112,38 @@ namespace GUI_Task
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(144, 373);
+            this.label1.Location = new System.Drawing.Point(110, 375);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(103, 13);
             this.label1.TabIndex = 6;
-            this.label1.Text = "label1";
+            this.label1.Text = "Motor Cycle (3 units)";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(144, 465);
+            this.label2.Location = new System.Drawing.Point(121, 465);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(68, 13);
             this.label2.TabIndex = 7;
-            this.label2.Text = "label2";
+            this.label2.Text = "Cars (5 units)";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(537, 375);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(86, 13);
             this.label3.TabIndex = 8;
-            this.label3.Text = "label3";
+            this.label3.Text = "Trucks (11 units)";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(537, 465);
+            this.label4.Location = new System.Drawing.Point(524, 465);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.Size = new System.Drawing.Size(96, 13);
             this.label4.TabIndex = 9;
-            this.label4.Text = "label4";
+            this.label4.Text = "Train Car (17 units)";
             // 
             // btnNewBoat
             // 
@@ -168,27 +168,24 @@ namespace GUI_Task
             this.lblCar.AutoSize = true;
             this.lblCar.Location = new System.Drawing.Point(144, 419);
             this.lblCar.Name = "lblCar";
-            this.lblCar.Size = new System.Drawing.Size(35, 13);
+            this.lblCar.Size = new System.Drawing.Size(0, 13);
             this.lblCar.TabIndex = 12;
-            this.lblCar.Text = "label6";
             // 
             // lblTruck
             // 
             this.lblTruck.AutoSize = true;
             this.lblTruck.Location = new System.Drawing.Point(537, 327);
             this.lblTruck.Name = "lblTruck";
-            this.lblTruck.Size = new System.Drawing.Size(35, 13);
+            this.lblTruck.Size = new System.Drawing.Size(0, 13);
             this.lblTruck.TabIndex = 13;
-            this.lblTruck.Text = "label7";
             // 
             // lblTarinCar
             // 
             this.lblTarinCar.AutoSize = true;
             this.lblTarinCar.Location = new System.Drawing.Point(537, 419);
             this.lblTarinCar.Name = "lblTarinCar";
-            this.lblTarinCar.Size = new System.Drawing.Size(35, 13);
+            this.lblTarinCar.Size = new System.Drawing.Size(0, 13);
             this.lblTarinCar.TabIndex = 14;
-            this.lblTarinCar.Text = "label8";
             // 
             // lblCapacityAndLoad
             // 
